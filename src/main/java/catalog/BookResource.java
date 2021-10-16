f9f2544257564a45de867a995fbb98c8e0ba77f7
Jacob Simpson <jacob.simpson@gmail.com>
2021-10-16 15:39:30 -0700

    Convert API to REST.

--- src/main/java/catalog/BookResource.java
+++ src/main/java/catalog/BookResource.java
@@ -1,16 +1,31 @@
 package catalog;
 
+import java.util.Set;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/books")
 public class BookResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+  public static class Book {
+    public String id;
+    public String title;
+    public String author;
+
+    public Book(String id, String title, String author) {
+      this.id = id;
+      this.title = title;
+      this.author = author;
     }
+  }
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public Set<Book> list() {
+    return Set.of(
+        new Book("kafj-83k1", "Distributed Services with Go", "Travis Jeffery"),
+        new Book("3ksi-91i3", "Designing Data-Intesive Applications", "Kleppmann"));
+  }
 }
--- src/test/java/catalog/BookResourceTest.java
+++ src/test/java/catalog/BookResourceTest.java
@@ -1,21 +1,28 @@
 package catalog;
 
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.quarkus.test.junit.QuarkusTest;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
 
 @QuarkusTest
 public class BookResourceTest {
 
-    @Test
-    public void testHelloEndpoint() {
-        given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
-    }
+  @Test
+  public void testHelloEndpoint() throws JsonProcessingException {
+    var want =
+        new ObjectMapper()
+            .writeValueAsString(
+                Set.of(
+                    new BookResource.Book(
+                        "kafj-83k1", "Distributed Services with Go", "Travis Jeffery"),
+                    new BookResource.Book(
+                        "3ksi-91i3", "Designing Data-Intesive Applications", "Kleppmann")));
 
+    given().when().get("/books").then().statusCode(200).body(is(want));
+  }
 }
